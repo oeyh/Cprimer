678f5a1eded6ede3234aff5a0834ab772037d116
oeyh - office <yanhai12345@gmail.com>
2015-10-01 13:29:48 -0500

    upload .gitignore file and files in chpt2

---
+++ chpt2/ex2-3.cpp
@@ -0,0 +1,18 @@
+// Ex2.3
+// type conversions
+#include <iostream>
+
+int main()
+{
+	unsigned u = 10, u2 = 42;
+	std::cout << u2 - u << std::endl;
+	std::cout << u - u2 << std::endl; // the size of int is usually 4 bytes
+	
+	int i = 10, i2 = 42;
+	std::cout << i2 - i << std::endl;
+	std::cout << i - i2 << std::endl;
+	
+	std::cout << i - u << std::endl;
+	std::cout << u - i << std::endl;
+	return 0;
+}
