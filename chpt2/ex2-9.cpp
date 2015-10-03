a4480de2ff4fb0f391dde900fc92472e92df1165
oeyh - Home <yanhai12345@gmail.com>
2015-10-03 00:19:34 -0500

    upload new files

---
+++ chpt2/ex2-9.cpp
@@ -0,0 +1,19 @@
+// Ex 2.9
+#include <iostream>
+
+int main()
+{
+    // int i = {3.14};
+    double i = {3.14};
+    int j = 3.14;
+    
+    double wage;
+    double salary = wage = 9999.99;
+    
+    // std::cin >> (int input_value);
+    
+    salary = wage * j * i;
+    std::cout << salary << std::endl;
+    std::cout << j << std::endl;
+    return 0;
+}
